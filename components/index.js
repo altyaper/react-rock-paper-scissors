3dabc86254e92172d10d9ff2d6878cb2d92ee59f
Jorge Chavez <jchavz@switchfly.com>
2019-05-18 12:32:05 -0600

    Center hello world

--- components/index.js
+++ components/index.js
@@ -6,7 +6,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="app">
         <h1>Hola mundo desde React</h1>
         <img src="/assets/images/react-logo.png"/>
       </div>
